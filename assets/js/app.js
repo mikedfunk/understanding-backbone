6f210d93ed660bed384f77c6327c9b75c8e8d546
Mike Funk <mike@mikefunk.com>
2015-01-05 10:48:11 -0800

    Separating DOM and Ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,26 @@
 // get jquery with browserify and browserify-shim
 var $ = require('jquery');
 
+/**
+ * add the status via ajax and add it to the list elements
+ *
+ * @param {object} the ajax options
+ * @return void
+ */
+function addStatus(options) {
+  $.ajax({
+    url: '/status/index.html',
+    type: 'GET',
+    dataType: 'html',
+    data: { text: $('#new-status').find('textarea').val() },
+    success: function(data) {
+      // when it succeeds, append the result to an li
+      $('#statuses>ul').append('<li>' + data + '</li>');
+      $('#new-status').find('textarea').val('');
+    }
+  });
+}
+
 // do this when the document is ready
 $(document).ready(function() {
   // on submitting the form
@@ -8,16 +28,6 @@ $(document).ready(function() {
     e.preventDefault();
 
     // call a route via ajax
-    $.ajax({
-      url: '/status/index.html',
-      type: 'GET',
-      dataType: 'html',
-      data: { text: $('#new-status').find('textarea').val() },
-      success: function(data) {
-        // when it succeeds, append the result to an li
-        $('#statuses>ul').append('<li>' + data + '</li>');
-        $('#new-status').find('textarea').val('');
-      }
-    });
+    addStatus();
   });
 });
