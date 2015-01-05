0d90427e603d9491007681154f0a4e98493ed90c
Mike Funk <mike@mikefunk.com>
2015-01-05 10:42:45 -0800

    added some comments for step 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,20 @@
-$ = require('jquery');
+// get jquery with browserify and browserify-shim
+var $ = require('jquery');
 
+// do this when the document is ready
 $(document).ready(function() {
+  // on submitting the form
   $('#new-status form').submit(function(e) {
     e.preventDefault();
 
+    // call a route via ajax
     $.ajax({
       url: '/status/index.html',
       type: 'GET',
       dataType: 'html',
       data: { text: $('#new-status').find('textarea').val() },
       success: function(data) {
+        // when it succeeds, append the result to an li
         $('#statuses>ul').append('<li>' + data + '</li>');
         $('#new-status').find('textarea').val('');
       }
